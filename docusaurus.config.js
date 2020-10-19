6ea8e89c1a2620085397caf964e11d318fc7b2f0
rienkdekok <67026900+rienkdekok@users.noreply.github.com>
2020-10-19 18:10:39 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,19 +41,19 @@ module.exports = {
     },
     footer: {
       style: 'dark',
-//      links: [
-//        {
-//          title: 'Docs',
-//          items: [
-//            {
-//              label: 'Style Guide',
-//              to: 'docs/',
-//            },
-//            {
-//              label: 'Second Doc',
-//              to: 'docs/doc2/',
-//            },
-//          ],
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
         },
 //        {
 //         title: 'Community',

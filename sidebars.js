e06686d80843d7e5daa194a04e0ef538392948d6
rienkdekok <rienk.de.kok@the-unit.eu>
2020-10-20 22:27:20 +0200

    sbp

--- sidebars.js
+++ sidebars.js
@@ -1,34 +1,17 @@
 module.exports = {
-//Introduction: ['introduction',],
-  dnajdha: [
-  {
-	Introduction: ['introduction',],
-  },
-   {
-     type: 'category',
-     label: 'Sectie I',
-     collapsed: true,
-     items: [
-        'doc1',
-        ],
-   },
-   {
-     type: 'category',
-     label: 'Sectie II',
-     collapsed: true,
-      items: [
-		'doc2',
-    		{
-      		type: 'category',
-      		label: 'Getting Started',
-      		collapsed: true,
-      		items: [
-			'doc4',
-			],
-    		},
-		'doc3',
-	], 
-   },
-  ],
-  about: ['about',],
+  docsrandomname: [ 'introduction',
+],
+
+//  aboutrandomname: ['about',],
+
+  ruimoprandomname: [ 'ruimop/ruimop',
+ 	{
+	     type: 'category',
+    	     label: 'Sectie I',
+	     collapsed: true,
+	     items: [
+	        'doc1','doc2',
+	        ],
+   	},
+   ], //ruimop
 };

d9592c9ca2ff9059f1fa90f9fb87ea0c631f3cf5
rienkdekok <rienk.de.kok@the-unit.eu>
2020-10-24 11:13:49 +0200

    doc.conf.js fixing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,33 +8,30 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
+    title: 'Overzicht',
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
+      Algemeen en verhelderend
+	</>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Inzicht',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      Verdieping 
+	</>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Details',
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+       Details en meer van dat 
+	</>
     ),
   },
 ];
@@ -59,7 +56,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
@@ -72,8 +69,8 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
+		 Naar overzicht Digitale Transformatie
+		</Link>
           </div>
         </div>
       </header>
